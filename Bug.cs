74f56f35af098a6993200763c2b589d0995d6fac
BugHunter-X <askzenai@gmail.com>
2025-01-05 16:50:07 +0100

    Add Bug.cs file

---
+++ Bug.cs
@@ -0,0 +1,19 @@
+public class ExampleClass
+{
+    public int MyProperty { get; set; }
+
+    public ExampleClass(int value)
+    {
+        MyProperty = value;
+    }
+
+    public static void Main(string[] args)
+    {
+        ExampleClass instance1 = new ExampleClass(10);
+        ExampleClass instance2 = instance1;
+
+        instance2.MyProperty = 20; 
+
+        Console.WriteLine(instance1.MyProperty); //Outputs 20
+    }
+}
